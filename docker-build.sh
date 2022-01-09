7c05256c624247b83bfbf4bb91210a5523dc1b30
Andrew Leichter <andrew.leichter@epsoftinc.com>
2022-01-09 08:52:28 -0500

    Adding google analytics stream tracking

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export JEKYLL_VERSION=3.5
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:$JEKYLL_VERSION jekyll build
+docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:$JEKYLL_VERSION "gem install jekyll-analytics; jekyll build"
